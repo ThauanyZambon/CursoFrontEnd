4f85d2caec35b4886d19d52ae7e3bdaef4acb925
ThauanyZambon <thauanyzambon2007@gamil.com>
2025-02-06 11:28:10 -0300

    06.02.2025 indrotucaojs

---
+++ IndroducaoJs/script00.js
@@ -0,0 +1,9 @@
+//let, ver, const
+
+let resultado = true; //stribuicao
+resultado = false; //reatricuicao
+//let resultado = false - proibido rescrever
+//let pode restribuir mas nao pode reescrever 
+
+var nota = 10; //atribuicao
+nota = 9; //reatribuicao
