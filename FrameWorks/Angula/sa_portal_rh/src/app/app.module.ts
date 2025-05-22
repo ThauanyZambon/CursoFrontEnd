637927a710f7559cca2c91b0f7f8d35aca18248a
ThauanyZambon <thauanyzambon2007@gamil.com>
2025-05-22 11:29:29 -0300

    22.05

---
+++ FrameWorks/Angula/sa_portal_rh/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InicioComponent } from './view/inicio/inicio.component';
+import { CurriculosComponent } from './view/curriculos/curriculos.component';
+import { VagasComponent } from './view/vagas/vagas.component';
+import { PainelVagasComponent } from './view/painel-vagas/painel-vagas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    CurriculosComponent,
+    VagasComponent,
+    PainelVagasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
